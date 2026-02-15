00d6a25560b53e6ee553d7116ce081f2d2f8f59d
Deep Shekhar Singh <deepshekhar0306@gmail.com>
2026-02-16 00:11:21 +0530

    fix: update beforeLoad function to throw redirect instead of returning it

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -19,8 +19,8 @@ import { createFileRoute, redirect } from '@tanstack/react-router';
 import { navRoutes } from '@/config/navRoutes';
 
 export const Route = createFileRoute('/')({
-  beforeLoad: (): ReturnType<typeof redirect> => {
-    return redirect({
+  beforeLoad: () => {
+    throw redirect({
       to: navRoutes[0].to,
     });
   },
